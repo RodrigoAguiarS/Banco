59cf422d7e3120acb3a807f7b39bdb61f1356809
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-11-29 21:18:55 -0300

    Conceitos essenciais e visão geral da tela a ser construida

--- app/src/main/java/br/com/rodrigo/banco/MainActivity.kt
+++ app/src/main/java/br/com/rodrigo/banco/MainActivity.kt
@@ -2,10 +2,16 @@ package br.com.rodrigo.banco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvList: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    private fun starViews(){
+
     }
 }
